4ccd0c9add0897ffe75abc79215c14fa799df2a0
Esraa El-Kheshen <79907447+EsraaElkheshen@users.noreply.github.com>
2025-05-06 01:22:46 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,13 +4,13 @@ pipeline {
     stages {
         stage('Get Code') {
             steps {
-                git url: 'https://github.com/EsraaElkheshen/Restful-Project.git', changelog: false, poll: false
+                git changelog: false, poll: false,  url: 'https://github.com/EsraaElkheshen/Restful-Project.git'
             }
         }
 
         stage('Run Test') {
             steps {
-                bat 'newman run  Restful-Booker.postman_collection.json -e RestFull_Testing.postman_environment.json --reporters=cli,htmlextra'
+                bat 'newman run  "Restful-Booker.postman_collection.json" -e "RestFull_Testing.postman_environment.json" --reporters=cli,htmlextra'
             }
         }
     }
